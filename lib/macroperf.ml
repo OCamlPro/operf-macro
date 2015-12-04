f2e05d17531dcde2bb3b9c97b235882683fb095e
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-12-04 18:20:40 +0900

    Better ordering of bench topics

--- lib/macroperf.ml
+++ lib/macroperf.ml
@@ -243,20 +243,20 @@ module Topic = struct
   module Gc = struct
     type t =
       | Minor_words
-      | Promoted_words
       | Major_words
+      | Promoted_words
+      | Top_heap_words
       | Minor_collections
       | Major_collections
+      | Compactions
       | Heap_words
       | Heap_chunks
-      | Top_heap_words
       | Live_words
       | Live_blocks
       | Free_words
       | Free_blocks
       | Largest_free
       | Fragments
-      | Compactions
     with sexp
 
     let of_string_exn : string -> t = function
@@ -383,7 +383,17 @@ module Topic = struct
     | List [Atom "Size"; t] -> Topic (Size.t_of_sexp t, Size)
     | _ -> invalid_arg "t_of_sexp"
 
-  let compare = Pervasives.compare
+  let compare a b = match a, b with
+    | Topic (a, Time), Topic (b, Time) -> Time.compare a b
+    | Topic (a, Gc), Topic (b, Gc) -> Gc.compare a b
+    | Topic (a, Size), Topic (b, Size) -> Size.compare a b
+    | Topic (a, Perf), Topic (b, Perf) -> String.compare a b
+    | Topic (_, Time), _ -> -1
+    | _, Topic (_, Time) -> 1
+    | Topic (_, Size), _ -> -1
+    | _, Topic (_, Size) -> 1
+    | Topic (_, Gc), _ -> -1
+    | _, Topic (_, Gc) -> 1
 end
 
 
